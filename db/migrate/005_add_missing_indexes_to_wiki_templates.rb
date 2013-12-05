e9749e9ee7f3b943797211ccc658d7ec08967179
cferbar <sfhynx.foros@gmail.com>
2013-12-05 10:05:55 +0000

    Columna repetida

--- db/migrate/005_add_missing_indexes_to_wiki_templates.rb
+++
@@ -1,9 +0,0 @@
-class AddMissingIndexesToWikiTemplates < ActiveRecord::Migration
-  def self.up
-    add_column :wiki_templates, :visible_children, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :wiki_templates, :visible_children
-  end
-end
